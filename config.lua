41c65002df0574b97e38f0e5fe4c25b52d77ffe6
ShinxD92 <52611202+ShinxD92@users.noreply.github.com>
2021-08-14 12:53:00 -0500

    Update config.lua

--- config.lua
+++ config.lua
@@ -1,7 +1,7 @@
 Config = {}
 
 Config.Webhooks = {
-    ["debug"] ="https://discord.com/api/webhooks/871456018759036978/TD78-Rxyr0P99R2RuGxIfVqMWNeWBvAieyEzvDoBqmsL2VuaZ0pQRp7sqp8R4lMZXvvH"
+    ["debug"] ="webhooks discord"
 }
 
 Config.Colors = {
